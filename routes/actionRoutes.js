8f0d272971199cb783c758fc9566824c7ee0f2cb
KROHNICK <business.kanekperez@gmail.com>
2019-02-15 10:33:09 -0800

    Actions post endpoint

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -32,4 +32,29 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  if (!req.body.project_id || !req.body.description || !req.body.notes) {
+    res.status(400).json({
+      message: "Please provide project id, description and notes."
+    });
+  }
+  try {
+    let projectId = await projectModel.get(req.body.project_id);
+    let newActReq = {
+      project_id: req.body.project_id,
+      description: req.body.description,
+      notes: req.body.notes
+    };
+    if (req.body.completed) {
+      newActReq.completed = req.body.completed;
+    }
+    let newAct = await actionModel.insert(newActReq);
+    res.status(201).json({ newAct });
+  } catch (err) {
+    res.status(500).json({
+      message: "Could not add new action."
+    });
+  }
+});
+
 module.exports = router;
